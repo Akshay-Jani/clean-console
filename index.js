ac2a2f7f573c441dadc2062dab129500faf92bd9
Akshay <akshayjani1999@gmail.com>
2024-04-17 22:59:35 +0530

    Add new keywords

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ if(getArgs(process.argv)){
     let rootPath = process.cwd();
     cleanDirectory(rootPath);
 }
+
+//To-do: Add Ts support
+//To-do: Add support to remove 'debugger' statements as well
